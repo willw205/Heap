fbb9adc6e049549026e5b33ad2fd21982ad39d5b
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-03-03 08:54:05 -0800

    Intializing variables and giving the user the option to choose between file input or user input

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+
+ #include <cstring>
+ #include <iostream>
+ #include <fstream>
+ using namespace std;
+
+ int main() {
+
+   char input;
+   int length = 0;
+   int* heap = new int[100];
+
+   for(int i = 0; i < 100; i++) {
+
+     heap[i] = 0;
+     
+   }
+
+   cout << "Please choose how you want to enter your numbers" << endl;
+   cout << "Type 1 for a file input or Type 2 for a user input!" << endl;
+   cin >> input;
+
+   
+   
+ }
