531fe7847da06e45a3e72605115bb2ae8502606c
William Wong <williamwong@Williams-MacBook-Pro.local>
2023-03-03 13:46:22 -0800

    Giving the user option for file input or user input

--- Main.cpp
+++ Main.cpp
@@ -20,6 +20,55 @@
    cout << "Type 1 for a file input or Type 2 for a user input!" << endl;
    cin >> input;
 
+   if(input == 1) {
+
+     char theFile[100];
+     int count = 0;
+     cout << "Please enter your file name" << endl;
+     cin >> theFile;
+     ifstream theFile;
+     numFile.open(theFile);
+
+     while(theFile >> heap[count]) {
+
+       cout << heap[count] << endl;
+       length++;
+       count++;
+       
+     }
+     theFile.close();
+     //Enter heapSort here
+   } else if (input == 2) {
+
+     int inputs = 0;
+
+     for(int i = 0; i < 100; i++) {
+
+       cout << "Please enter a number between 1 and 1000 and you can input -1 to terminate the loop" << endl;
+       cin >> inputs;
+       if(inputs = -1) {
+
+	 break;
+       } else {
+
+	 heap[i] = inputs;
+	 length++;
+       }
+     }
+     //Enter heapsport hear
+     
+   } else {
+
+     cout << "Not Valid!" << endl;
+     
+   }
+
+   for(int i = 0; i < 100; i++) {
+
+     cout << heap[i] << " ";
+     
+   }
+
    
    
  }
